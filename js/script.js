ff71a0f44171769879993f3e306c19712e435e2b
mrefaldorachman <refaldodev@gmail.com>
2020-06-14 00:13:51 +0700

    berhasil buat button to top

--- js/script.js
+++ js/script.js
@@ -271,9 +271,23 @@ fetch(
     });
 
 // for(a= 0; a<j; a++){
-console.log(Jumlah_Mat);
-// }
 
+// }
+// back to top
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('#topBtn').fadeIn();
+        } else {
+            $('#topBtn').fadeOut();
+        }
+    });
+    $('#topBtn').click(function () {
+        $('html, body ').animate({
+            scrollTop: 0
+        }, 800);
+    });
+});
 
 
 function call_hightchart() {
