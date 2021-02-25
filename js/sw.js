a8c9e240445db6f209492042536f6876b495c837
mrefaldorachman <refaldodev@gmail.com>
2021-02-25 20:17:50 +0700

    ngerapihin file

--- js/sw.js
+++ js/sw.js
@@ -3,10 +3,10 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
       navigator.serviceWorker.register('/sw.js').then(function(registration) {
         // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        // console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
         // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
+        // console.log('ServiceWorker registration failed: ', err);
       });
     });
   }
@@ -25,7 +25,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
+        // console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
