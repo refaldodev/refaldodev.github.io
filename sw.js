28b41aa3fb1a024cf67f16cc66ce5b1b587f0af4
refaldodev <m.refaldorachman@gmail.com>
2021-05-02 15:30:18 +0700

    add dark mode btn toggle version

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ if ('serviceWorker' in navigator) {
   }
 
 // chache
-  var CACHE_NAME = 'refaldo-cache-v4.4.1';
+  var CACHE_NAME = 'refaldo-cache-v4.4.0';
 var urlsToCache = [
   '/',
     '/index.html',
